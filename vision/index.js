a2fd5fcde49ea94ff53fc71a37320587d9096c48
Jhemeson Silva Mota <jhemesonmotta@gmail.com>
2018-06-01 19:20:50 -0300

    Initial Commit - Vision Module

---
+++ vision/index.js
@@ -0,0 +1,24 @@
+var request = require("request");
+var cheerio = require("cheerio");
+module.exports.getFotos = function(url, callback) {
+  request(url, function(error, response, html) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+      var fotos = [];
+
+      $( "img" ).each(function( index ) {
+        var img = $( this ).attr('src');
+        if(img.includes("http")){
+          fotos.push(img);
+        }
+      });
+      
+      callback(fotos);
+    }
+    else{
+      console.log("err: " + error);
+      console.log("statusCode: " + response.statusCode);
+    }
+  });
+
+}
