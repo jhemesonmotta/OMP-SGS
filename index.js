770c18fcf3e1d7bf7f0a40be94e48828a3c8b424
Alexandre Henrique Kavalerski Teixeira <kavalerskialexandre@gmail.com>
2018-06-14 11:17:44 -0300

    Adicionando integração com Facebook

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var request = require("request");
 var vision = require('./vision');
 var bodyParser = require('body-parser');
+var firebase = require("firebase");
+
 var app = express();
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -15,28 +18,28 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.render('pages/index')
 });
 
-app.get('/saiba-mais', function(request, response) {
+app.get('/saiba-mais', function (request, response) {
   response.render('pages/saibamais')
 });
 
-app.get('/sobre-nos', function(request, response) {
+app.get('/sobre-nos', function (request, response) {
   response.render('pages/sobrenos')
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
 
-app.post('/', function(request, response){
+app.post('/', function (request, response) {
   console.log("aqui");
   var perfil = request.body.urlperfil;
   console.log("url: " + perfil);
 
-  vision.getFotos(perfil, function(data) {
-    response.render('pages/resultado', {fotos: data} ); 
+  vision.getFotos(perfil, function (data) {
+    response.render('pages/resultado', { fotos: data });
   });
 });
